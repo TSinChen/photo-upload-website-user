2782cdad709e2ad93bc49cdc5d1bf2acdff97ace
TSin Chen <sinin60076@gmail.com>
2022-01-03 16:42:01 +0800

    image zoom

--- src/components/List/List.tsx
+++ src/components/List/List.tsx
@@ -4,20 +4,15 @@ import {
   ImageListItem,
   ImageListItemBar,
   Typography,
-  Dialog,
-  DialogTitle,
-  DialogContent,
 } from '@mui/material'
+import Zoom from 'react-medium-image-zoom'
+import 'react-medium-image-zoom/dist/styles.css'
 
 import apis from '../../apis/apis'
 import { Post } from '../../types/post'
 
 const List = () => {
   const [posts, setPosts] = useState<Post[]>([])
-  const [dialogImage, setDialogImage] = useState<{ open: boolean; post: Post | null }>({
-    open: false,
-    post: null,
-  })
 
   const fetchPosts = async () => {
     try {
@@ -28,14 +23,6 @@ const List = () => {
     }
   }
 
-  const openDialogImage = (index: number) => {
-    setDialogImage({ open: true, post: posts[index] })
-  }
-
-  const closeDialogImage = () => {
-    setDialogImage({ open: false, post: null })
-  }
-
   useEffect(() => {
     fetchPosts()
   }, [])
@@ -43,14 +30,11 @@ const List = () => {
   return (
     <Fragment>
       <ImageList variant="masonry" cols={3} gap={20} sx={{ maxWidth: '1200px', margin: '40px auto' }}>
-        {posts.map((post, index) => (
+        {posts.map((post) => (
           <ImageListItem key={post._id}>
-            <img
-              src={post.imageLink}
-              loading="lazy"
-              style={{ cursor: 'zoom-in' }}
-              onClick={() => openDialogImage(index)}
-            />
+            <Zoom>
+              <img src={post.imageLink} loading="lazy" style={{ width: '100%' }} />
+            </Zoom>
             <ImageListItemBar
               title={<Typography variant="h6">{post.title}</Typography>}
               subtitle={<Typography variant="body2">{post.description}</Typography>}
@@ -58,20 +42,6 @@ const List = () => {
           </ImageListItem>
         ))}
       </ImageList>
-      <Dialog open={dialogImage.open} onClose={closeDialogImage}>
-        <DialogTitle>
-          <Typography variant="h5" my={1}>
-            {dialogImage.post?.title}
-          </Typography>
-        </DialogTitle>
-        <DialogContent>
-          <img
-            src={dialogImage.post?.imageLink}
-            style={{ width: '100%', height: '100%', cursor: 'zoom-out' }}
-            onClick={closeDialogImage}
-          />
-        </DialogContent>
-      </Dialog>
     </Fragment>
   )
 }
